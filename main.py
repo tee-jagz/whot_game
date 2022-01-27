3d12d386bfb642f021f3675118a68d172b5a774f
tee-jagz <jegstolu@gmail.com>
2022-01-27 02:17:02 +0000

    Progress

--- main.py
+++ main.py
@@ -51,6 +51,9 @@ class deck:
     def give(self):
         return self.deck.pop()
 
+    def remainingCards(self):
+        return len(self.deck)
+
     def show(self):
         for card in self.deck:
             print(card.cardDisplay())
@@ -61,6 +64,7 @@ class hand:
     def __init__(self, name):
         self.hand = []
         self.name = name
+        self.isSkip = False
 
     def getCard(self, num):
         for i in range(num):
@@ -85,8 +89,8 @@ class hand:
             print(f'({ind}):{card.cardDisplay()}', end=' | ')
         print('\n')
 
-    def skip(self, isSkip = False):
-        return isSkip
+    def skip(self, isSkip = True):
+        self.isSkip = isSkip
 
     def liability(self):
         temp = 0
@@ -121,71 +125,104 @@ class engine:
     def startGame(self):
         deck.shuffle()
         for player in self.players:
-            player.getCard(8)
+            player.getCard(4)
         stage.addCard(deck.give())
 
+    def punish(self, player):
+        player.getCard(stage.getStage().cardAction())
+
+    def checkDeck(self, players):
+        if deck.remainingCards() < 1:
+            temp = {}
+            for player in players:
+                temp[player.name] = player.liability()
+                print(f"{player.name} : {player.liability()}", end = ' | ')
+                
+            k = list(temp.values())
+            winValInd = k.index(min(k))
+            winner = list(temp.keys())[winValInd]
+            print(f"\n{winner} won!\n\n")
+            return True
+
     def play(self):
-        turn = 0
+        count = 0
         while True:
-            print('Turn :', turn)
+            turn = count % len(self.players)
+            next = (turn+1) % len(self.players)
+
             if turn == 0:
                 self.players[turn].show()
+            
+            if self.players[turn].isSkip:
+                count += 1
+                continue
+
+            #self.players[turn].show()
             stage.show()
+            print(f"\n{self.players[next].name} : {self.players[next].remainingCards()} card(s) left")
+            print(f"Deck : {deck.remainingCards()}\n{'_'*20}")
             if turn == 0:
                 while True:
                     pick = input('Which card will you like to play: ')
+                    
                     if (pick.isdigit() == False) or (int(pick) > len(self.players[turn].hand) and int(pick) != 100):
                         print('Enter the index of card you want to play or "100" to pick a card.')
                         continue
+                    elif int(pick) == 100:
+                        self.players[(turn+1) % len(self.players)].skip(False)
+                        break
                     else:
                         if self.players[turn].checkStage(self.players[turn].hand[int(pick)], stage.getStage()) == False:
                             print('Select a card that matches shape or number or "100" to pick a card.')
                             continue
                         break
-                self.players[turn].playCard(int(pick))
+                
+                if int(pick) == 100:
+                    self.players[turn].getCard(1)
+                else:
+                    self.players[turn].playCard(int(pick))
+                    if stage.getStage().isAction() == True:
+                        self.players[(turn+1) % len(self.players)].skip()
+                        self.punish(self.players[next])
+                    else:
+                        self.players[next].skip(False)
+            
             else:
                 c = 0
                 while True:
                     pick = random.randint(0, self.players[turn].remainingCards()-1)
 
                     if self.players[turn].checkStage(self.players[turn].hand[int(pick)], stage.getStage()) == False:
-                        print(f'{pick} Computer dey select rubbish lowo.')
+                        # print(f'{pick} Computer dey select rubbish lowo.')
                         c += 1
                         if c == 14:
-                            self.players[turn].show()
+                            self.players[turn].getCard(1)
+                            self.players[next].skip(False)
+
                             break
                         continue
                     else:
                         self.players[turn].playCard(pick)
+                        if stage.getStage().isAction():
+                            self.players[next].skip()
+                            self.punish(self.players[next])
+                        else:
+                            self.players[next].skip(False)
                         break
-                    
+            if self.checkDeck(self.players):
+                break
             if self.players[turn].remainingCards() < 1:
-                print('Game finished!')
+                print(f'{self.players[turn].name} won!')
                 break
             else:
-                print('Turn: ', turn)
-                if turn == (len(self.players)-1):
-                    turn = 0
-                else:
-                    turn += 1
-
-            
-                
-
-
+                count +=1
 
 
 stage = stage()
 deck = deck()
 engine = engine(2)    
 
-# pHand = hand('Player 1')
-# cHand = hand('AI 1')
-# stage.addCard(deck.give())
 print(f'{"-"*100}\nWelcome to the game of WORT!\n{"-"*70}\n')
-# while True:
-#     pHand.getCard(4)
-#     cHand.getCard(4)
 
 engine.startGame()
 engine.play()
